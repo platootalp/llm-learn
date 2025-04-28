5f3086128da33b852871720e92b22b56648de512
lijunyi <lijunyi3@tuhu.cn>
2025-04-28 12:04:12 +0800

    feat: langgraph

--- util/llm_factory.py
+++ util/llm_factory.py
@@ -3,17 +3,24 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from getpass import getpass
 
-load_dotenv()
 
 def get_qwen_model():
     """
     获取 Qwen 模型实例（单例/懒加载也可扩展）
     """
-    if not os.environ.get("QWEN_API_KEY"):
-        os.environ["QWEN_API_KEY"] = getpass("Enter API key for Qwen: ")
+    load_dotenv()
+    # if not os.environ.get("QWEN_API_KEY"):
+    #     os.environ["QWEN_API_KEY"] = getpass("Enter API key for Qwen: ")
 
     return ChatOpenAI(
         model_name="qwen-turbo",
         openai_api_base="https://dashscope.aliyuncs.com/compatible-mode/v1",
-        openai_api_key=os.environ["QWEN_API_KEY"]
+        openai_api_key=os.environ["DASHSCOPE_API_KEY"]
     )
+
+if __name__ == '__main__':
+    model = get_qwen_model()
+    chunks = []
+    for chunk in model.stream("what color is the sky?"):
+        chunks.append(chunk)
+        print(chunk.content, end="|", flush=True)
